fd53366057520dc7aaa09653e7464fc9cdd0f504
adityaatre26 <aditya.atre26@gmail.com>
2025-05-26 01:14:25 +0530

    Project deletion route done

---
+++ app/api/projects/[projectId]/delete-project/route.ts
@@ -0,0 +1,53 @@
+import { PrismaClient } from "@prisma/client";
+import axios from "axios";
+const prisma = new PrismaClient();
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: { projectId: string } }
+) {
+  try {
+    const { UserId } = await request.json();
+    const { projectId } = await params;
+
+    const user = await prisma.user.findUnique({
+      where: { UserId: UserId },
+    });
+
+    if (!user) {
+      return new Response("User not found", { status: 404 });
+    }
+
+    const repoTBD = await prisma.repos.findUnique({
+      where: { projectId: projectId },
+    });
+
+    if (!repoTBD) {
+      return new Response("Project not linked to a repo", { status: 404 });
+    }
+
+    await axios.delete(
+      `http://localhost:3000/api/projects/${projectId}/delete-repo`,
+      {
+        data: {
+          UserId: UserId,
+          repoId: repoTBD.id,
+        },
+      }
+    );
+
+    // Delete the project from the database
+    const deletedP = await prisma.project.delete({
+      where: {
+        ProjectId: projectId,
+      },
+    });
+
+    console.log("Deleted project:", deletedP);
+
+    return new Response("Project deleted successfully", { status: 200 });
+  } catch (error) {
+    console.error("Error deleting project:", error);
+    return new Response("Failed to delete project", { status: 500 });
+  }
+}
